68e8f801678274c123974d8c8e1398a5dd20ba3e
mauric <mauric_rck@hotmail.com>
2020-08-10 09:34:57 -0300

    delete from .gitignore extra lines

---
+++ database/config/config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  "development": {
+    "username": "mauric",
+    "password": "YvpgjseY5cCHKNJ",
+    "database": "neoposdb",
+    "host": "35.192.56.243",
+    "dialect": "mysql"
+  },
+  "test": {
+    "username": "root",
+    "password": null,
+    "database": "database_test",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  },
+  "production": {
+    "username": "root",
+    "password": null,
+    "database": "database_production",
+    "host": "127.0.0.1",
+    "dialect": "mysql"
+  }
+}
