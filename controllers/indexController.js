d6a4be626656923941c746c05ea12791e28b19cf
mauric <mauric_rck@hotmail.com>
2020-08-09 22:17:09 -0300

    structure

---
+++ controllers/indexController.js
@@ -0,0 +1,13 @@
+const db = require('../database/models');
+//TODO CAMBIAR TODO
+let indexController = {
+    index: async ( req , res , next  ) => {
+        let shops = await db.Shop.findAll() 
+        res.render( 'index' ,{ user : req.session.user , shops: shops });
+    },
+    mall: async ( req , res , next  ) => {
+        let shops = await db.Shop.findAll() 
+        res.render( 'mall' ,{ user : req.session.user , shops: shops });
+    },
+}
+module.exports = indexController;
